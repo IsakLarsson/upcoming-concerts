826e66cf0d5afc514b866bc4224df382a1ba4035
Isak Larsson <isaklarsson98@hotmail.com>
2022-11-14 09:54:48 +0100

    refactor

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -2,12 +2,21 @@
 import playwright from 'playwright'
 import { getEventInfoFromCard } from '../eventInfoParser.js'
 const chromium = playwright.chromium
-let eventList
+let eventList = []
 
 const browser = await chromium.launch({
     headless: true, // setting this to true will not run the UI
 })
 
+const loadEvents = async (cards) => {
+    let loadedEvents = []
+    for await (const card of cards) {
+        const eventInfo = await getEventInfoFromCard(card)
+        loadedEvents.push(eventInfo)
+        console.log('Added event: ', eventInfo)
+    }
+    return loadedEvents
+}
 export const getEvents = async (req, res) => {
     let { city } = req.body
     if (!city) {
@@ -22,13 +31,9 @@ export const getEvents = async (req, res) => {
             `https://www.livenation.se/event/allevents?location=${city}&page=1&genres=`
         )
         const cards = await page.$$('section.result-card__wrapper')
-        for await (const card of cards) {
-            const eventInfo = await getEventInfoFromCard(card)
-            eventList.push(eventInfo)
-            console.log('Added event: ', eventInfo)
-        }
         console.log('Current event list: ', eventList)
         console.log('added event info')
+        eventList = await loadEvents(cards)
         if (eventList.length === 0) {
             res.status(400).json({ message: 'No events could be found' })
             return
